b464abf720292e4e82a09b7774ea5701bb75b97e
Mohammad Elshatarat <melshata@c2r4s11.42amman.com>
2026-06-11 21:41:15 +0300

    add tmp

--- parsing/parse_input.c
+++ parsing/parse_input.c
@@ -1,25 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-
-// #include "get_next_line/get_next_line.h"
-#include "libft/libft.h"
-
-
-typedef struct s_vars
-{
-	t_list	*env;
-	char	*line;
-	char	*word;
-	char	**arg_arr;
-	char	**path_arr;
-}			t_vars;
+#include "minishell.h"
 
 void	arr_free(char **arg_arr)
 {
